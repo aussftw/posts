c7e9c986d86c60a20f060f5ed754b05484fc8081
aussftw <shagronaus@gmail.com>
2019-02-24 21:37:39 +0200

    working with thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,9 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 export const fetchPosts = () => {
-  return function ( dispatch, getState ) {
-    const response = jsonPlaceholder.get('/posts');
+  return async  ( dispatch ) => {
+    const response = await jsonPlaceholder.get('/posts');
 
-    return {
-      type: 'FETCH_POSTS',
-      payload: response
-    };
+    dispatch({ type: 'FETCH_POSTS', payload: response })
   };
 };
